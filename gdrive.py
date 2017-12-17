63a22f21cfe1b815c8f70b51cda2e9b641f649f9
Raihat Zaman Neloy <raihat.zaman@newscred.com>
2017-12-18 03:36:23 +0600

    Add Download feature in the gdrive script

--- gdrive.py
+++ gdrive.py
@@ -18,6 +18,7 @@ drive = GoogleDrive(gauth)
 access_token = json.loads(open('gdriveauth').read())
 access_token = access_token['access_token']
 
+
 def list_file(folder_id='root', max_files=100000, output_file=None):
 	command = "./google-drive --access-token %s list -q '\"%s\" in parents and trashed=False' -m %d" % (access_token, folder_id, max_files)
 	
@@ -51,6 +52,30 @@ def list(id, url, mx, output_file):
 		list_file(id, mx, output_file)
 	elif not id and not url:
 		list_file()
+
+
+@cli.command()
+@click.option('--id', help='ID of the file to download')
+@click.option('-f', '--file', help='Specify file to read info regarding download')
+@click.option('-o', '--output-file', default='output/', help='Path to the directory where to save the file, default = ./output/*')
+def download(id, file, output_file):
+	to_download = []
+
+	if file:
+		with open(file) as inp_file:
+			file_infos = inp_file.readlines()
+
+			for line in file_infos:
+				words = line.split()
+
+				if words[0] != 'Id':
+					to_download.append(words[0])
+	if id:
+		to_download.append(id)
+
+	for id in to_download:
+		os.system('./google-drive --access-token %s download --path %s -f -r %s' % (access_token, output_file, id))
+
 
 if __name__ == "__main__":
 	cli()
